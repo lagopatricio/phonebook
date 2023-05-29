db10ae48edcb5c5cd2d200ccd9c04a208e00bea3
lagopatricio <dev.lagopatricio@gmail.com>
2023-05-29 15:50:33 -0300

    finish backend

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require('express');
-const app = express()
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(express.json());
+
+morgan.token('content', function getContent (req){
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.content(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+}));
 
 let phonebook = [
   { 
@@ -41,6 +60,31 @@ app.get('/api/phonebook/:id', (req, res) =>{
   const id = Number(req.params.id);
   const contact = phonebook.find(contact => contact.id === id);
   res.json(contact);
+})
+
+app.delete('/api/phonebook/:id', (req, res) =>{
+  const id = Number(req.params.id);
+  phonebook = phonebook.filter(contact => contact.id !== id);
+  res.status(204).end();
+})
+
+app.post('/api/phonebook', (req, res) =>{
+  const id = Math.floor(Math.random() * 10000000000) + 1;
+  const newContact = req.body;
+
+  if(!newContact.name){
+    return res.status(400).json({
+      error: 'Please input a name'
+    })
+  } else if(phonebook.find(contact => contact.name === newContact.name)){
+    return res.status(400).json({
+      error: `Contact with name ${newContact.name} already exists.`
+    })
+  }
+
+  newContact.id = id;
+  phonebook = phonebook.concat(newContact);
+  res.json(newContact);
 })
 
 const PORT = 3001;
