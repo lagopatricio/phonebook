d6f214c352cdcaf7c0c8802b3d0abbf7f7c59e51
lagopatricio <dev.lagopatricio@gmail.com>
2023-06-01 13:15:50 -0300

    add DB connectivity

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const Contact = require('./models/contact');
 
 const app = express();
 
@@ -21,84 +23,53 @@ app.use(morgan(function (tokens, req, res) {
   ].join(' ')
 }));
 
-let phonebook = [
-  {
-    'data':{
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    "id": 1
-  },
-  {
-    'data':{
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    "id": 2
-  },
-  { 
-    'data':{
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    "id": 3
-  },
-  { 
-    'data':{
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    },
-    "id": 4
-  }
-]
-
 app.get('/api/phonebook', (req, res) =>{
-  res.json(phonebook);
+  Contact.find({}).then(contacts => {
+    res.json(contacts);
+  })
 })
 
-app.get('/info', (req, res) =>{
-  res.send(
-    `
-    <p>Phonebook has info for ${phonebook.length} contacts.</p>
-    <p>${new Date()}</p>
-    `
-    )
-})
+// app.get('/info', (req, res) =>{
+//   res.send(
+//     `
+//     <p>Phonebook has info for ${phonebook.length} contacts.</p>
+//     <p>${new Date()}</p>
+//     `
+//     )
+// })
 
 app.get('/api/phonebook/:id', (req, res) =>{
-  const id = Number(req.params.id);
-  const contact = phonebook.find(contact => contact.id === id);
-  res.json(contact);
+  Contact.findById(req.params.id).then(contact => {
+    res.json(contact);
+  })
 })
 
-app.delete('/api/phonebook/:id', (req, res) =>{
-  const id = Number(req.params.id);
-  phonebook = phonebook.filter(contact => contact.id !== id);
-  res.status(204).end();
-})
+// app.delete('/api/phonebook/:id', (req, res) =>{
+//   const id = Number(req.params.id);
+//   phonebook = phonebook.filter(contact => contact.id !== id);
+//   res.status(204).end();
+// })
 
 app.post('/api/phonebook', (req, res) =>{
-  const id = Math.floor(Math.random() * 10000000000) + 1;
-  const newContact = req.body;
-  const newContactData = newContact.data;
+  const newContact = req.body.data;
 
-  if(!newContactData.name){
-    return res.status(400).json({
-      error: 'Please input a name'
-    })
-  } else if(phonebook.find(contact => contact.data.name === newContactData.name)){
-    return res.status(400).json({
-      error: `Contact with name ${newContactData.name} already exists.`
-    })
+  if (newContact.name.length < 1 || newContact.name === undefined){
+    return res.status(400).json({ error: 'Please input a name.' })
   }
 
-  req.body.id = id;
-  phonebook = phonebook.concat(newContact);
-  res.json(newContact);
+  const contact = new Contact({
+    data:{
+      name: newContact.name, 
+      number: newContact.number,
+    }
+  })
+
+  contact.save().then(savedContact => {
+    res.json(savedContact);
+  })
 })
 
-const PORT = process.env.PORT || 3001;
-
+const PORT = process.env.PORT
 app.listen(PORT, () =>{
   console.log(`Server running on port ${PORT}.`);
 })
---
+++ models/contact.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+
+const url = process.env.MONGODB_URI;
+
+console.log('Connecting to', url);
+
+mongoose.connect(url)
+  .then(res =>{
+    console.log('Connected to DB');
+  })
+  .catch((error) => {
+    console.log('Error connecting to DB', error.message);
+  })
+
+const contactSchema = new mongoose.Schema({
+  data:{
+    name: String, 
+    number: String,
+  }
+})
+
+contactSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+})
+
+module.exports = mongoose.model('Contact', contactSchema);
